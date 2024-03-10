182d0b97d7be8db7a3c77c3c8ee89a932af89172
js-easy-school <supergeroy11@gmail.com>
2024-03-10 23:19:37 +0300

    update

--- client/src/app/components/ui/registerForm.jsx
+++ client/src/app/components/ui/registerForm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { validator } from "../../utils/ validator";
+import { validator } from "../../utils/validator";
 import TextField from "../common/form/textField";
 import SelectField from "../common/form/selectField";
 import RadioField from "../common/form/radio.Field";
