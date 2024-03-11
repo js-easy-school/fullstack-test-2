bbe273aa814d49c4fa7de7716a73b009aafe54e3
js-easy-school <supergeroy11@gmail.com>
2024-03-12 00:14:40 +0300

    edit helpers.js

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -6,7 +6,7 @@ function generateUserData() {
   return {
     rate: getRandomInt(1, 5),
     completedMeetings: getRandomInt(0, 200),
-    image: `https://avatars.dicebear.com/api/avataaars/${(Math.random() + 1)
+    image: `https://api.dicebear.com/7.x/avataaars/svg/${(Math.random() + 1)
       .toString(36)
       .substring(7)}.svg`,
   }
